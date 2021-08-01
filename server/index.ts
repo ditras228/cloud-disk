1f6040c701a0c9c633ee4da664da58cfb6a868ef
Dmitry Druzinin <78543687+ditras228@users.noreply.github.com>
2021-08-01 19:26:12 +0300

    production ver

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const filePathMiddleware = require('./middleware/filepath.middleware.ts')
 const staticPathMiddleware = require('./middleware/staticPath.middleware.ts')
 const path = require('path')
 const app=express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 
 app.use(fileUpload({}))
