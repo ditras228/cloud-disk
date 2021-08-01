e22af42ece08eb9ed630cf51050f0686865a9987
Dmitry Druzinin <78543687+ditras228@users.noreply.github.com>
2021-08-01 19:27:55 +0300

    improve port

--- client/src/components/api/api.ts
+++ client/src/components/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = ' http://87.236.22.121:5000'
+export const baseURL = ' http://87.236.22.121:5001'
 export const instance = axios.create({
     baseURL: `${baseURL}/api`
 })
