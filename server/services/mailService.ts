fee28c12e7801b5208e24ed426dd4f66d681e0a2
Dmitry Druzinin <78543687+ditras228@users.noreply.github.com>
2021-08-05 23:00:58 +0300

    mail fix

--- server/services/mailService.ts
+++ server/services/mailService.ts
@@ -1,19 +1,19 @@
 const nodemailer = require("nodemailer");
 export async function main(to: string, hash: string) {
     let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
+        host: "smtp.beget.com",
+        port: 2525,
         secure: false,
         auth: {
-            user: 'druzhinindmitry2020@gmail.com',
-            pass: process.env.EMAILPASSWORD,
+            user: 'druzhinindmitry@druzhinindmitry.ru',
+            pass: process.env.EMAILPASSWORD
         },
     });
 
     let link=`http://localhost:3000/regconfirm?hash=${hash}`
 
     await transporter.sendMail({
-        from: '"MERNDisk" <druzhinindmitry2020@gmail.com>',
+        from: '"MERNDisk" <druzhinindmitry@druzhinindmitry.ru>',
         to: to,
         subject: "Подтверждение регистрации",
         text: "Спасибо за регистрацию на MERNDisk!",
